0236d5b1629aa8a609a4114d4eaf1047eb756b62
crazymugg <78958134+crazymugg@users.noreply.github.com>
2023-06-25 20:40:25 +0200

    templates init

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 def create_app():
     app = Flask(__name__)
+    bootstrap = Bootstrap5(app)
 
     @app.route('/')
     def index():
-        return 'Be Right Back!'
+        return render_template('base.html')
 
     return app
