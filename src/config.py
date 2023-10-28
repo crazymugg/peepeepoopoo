c56d9eb2d95fa57d7492d5924b7acf25a28a682d
crazymugg <crazymugg.gaming@gmail.com>
2023-10-27 20:21:55 -0500

    added svelte frontend

--- src/config.py
+++
@@ -1,3 +0,0 @@
-class config():
-    SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
-    FLASK_ADMIN_SWATCH = 'cerulean'
--- src/db.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-def create_db():
-    db = SQLAlchemy()
-
-
-    return db 
